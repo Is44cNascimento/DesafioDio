57c101ea617735c27fe4062b598cfa47a84a5f72
Is44cNascimento <isaacgamex7@outlook.com>
2025-03-30 11:52:35 -0300

    30-03 : Subindo o basico

---
+++ src/Conta/DadosBancarios.java
@@ -0,0 +1,62 @@
+package Conta;
+
+public class DadosBancarios {
+
+    private int numero;
+    private String agencia;
+    private String nomeCliente;
+    private double saldo;
+
+    public DadosBancarios() {
+    }
+
+    public DadosBancarios(int numero, String agencia, String nomeCliente, double saldo) {
+        this.numero = numero;
+        this.agencia = agencia;
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;
+    }
+
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getAgencia() {
+        return this.agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getNomeCliente() {
+        return this.nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public void entrarConta(int numero,String agencia, String nomeCliente, double saldo) {
+
+
+
+
+    }
+
+
+
+}
